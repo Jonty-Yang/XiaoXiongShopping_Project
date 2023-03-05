ad337fae971aa913cfadec7d967086502fafc023
ahao <jontyyang@126.com>
2023-03-05 13:24:17 +0800

    ID封装类TableId的编写

---
+++ src/main/java/com/jonty/b2b/util/TableId.java
@@ -0,0 +1,30 @@
+package com.jonty.b2b.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * 创建作者: AHao
+ * 创建日期: 2023/3/5 13:20
+ * 项目名称: xiao_xiong_shopping
+ */
+
+
+/** ID 封装类*/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TableId {
+
+    private List<Id> ids;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Id {
+        private Integer id;
+    }
+}
